9b4ef06219d3fa4370f2b839485de453326c3fae
ashwindhevaraj <ashwindhevaraj@gmail.com>
2024-02-16 22:59:39 +0530

    implementedselectdropdown

--- Testcases/loginpage.py
+++ Testcases/loginpage.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class loginpage(unittest.TestCase):
     def setUp(self):
         self.driver=webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        
     def test_openbrowser(self):
         driver=self.driver
         #self.driver=webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -22,7 +23,16 @@ class loginpage(unittest.TestCase):
         self.elem2.send_keys("admin123")
         self.elem3=self.driver.find_element(By.CSS_SELECTOR,"[type='submit']")
         self.elem3.click()
+        self.elem4=self.driver.find_element(By.XPATH,"//span[text()='Directory']")
+        self.elem4.click()
+        self.elem5=self.driver.find_element(By.XPATH,"(//div[@class='oxd-select-text oxd-select-text--active'])[1]")
+        self.elem5.click()
+        self.elem6=self.driver.find_elements(By.XPATH,"//div[@role='listbox']//div[@role='option']")
+        for x in self.elem6:
+            print(x.text)
         self.assertIn("Aswin","Aswin")
+                
+
     
     def tearDown(self):
         self.driver.quit()
